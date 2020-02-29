5741760cfef65732bc74f99d75e0d9e06f35fbaf
sa992 <sa42@illinois.edu>
2020-02-28 22:56:14 -0600

    Initial Commit

---
+++ Ractual/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ractual
+//
+//  Created by Shivani Agrawal on 2/28/20.
+//  Copyright © 2020 Shivani Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
