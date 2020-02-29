9db24d705837ef9703eb5c38a5c2384639ff1bcf
sa992 <sa42@illinois.edu>
2020-02-29 00:10:44 -0600

    added buttons for friends

---
+++ Ractual/FriendsGames.swift
@@ -0,0 +1,28 @@
+//
+//  FriendsGames.swift
+//  Ractual
+//
+//  Created by Shivani Agrawal on 2/29/20.
+//  Copyright © 2020 Shivani Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class FriendsGames: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
